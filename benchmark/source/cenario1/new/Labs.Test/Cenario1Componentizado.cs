bded427a3de0d5670d72477e228b5c555a77eac8
drr00t <adribeiro@gmail.com>
2020-01-26 22:38:30 -0300

    implementação do fluxo de ida e volta com 3 camadas, as interfaces do Halder e Executor já estão definidas em termos de camada de transaporte, agora vou generalizar o código e transforma uma classe genŕica para receber a lógica o cliente ainda não está legal, preciso pensar mais sobre como a api será criada, talvez como uma interface com um único método genérico, talvez como operadores Filter, Sum, etc.

--- benchmark/source/cenario1/new/Labs.Test/Cenario1Componentizado.cs
+++ benchmark/source/cenario1/new/Labs.Test/Cenario1Componentizado.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Client;
 using NetMQ;
 using NetMQ.Sockets;
+using Newtonsoft.Json;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -25,79 +28,279 @@ namespace Labs.Test
             
             var endpointVentilator = "inproc://127.0.0.1:5100";
             
-            var command = new RouterSocket();
-            command.ReceiveReady += (sender, args) =>
-            {
-                _testOutputHelper.WriteLine("New connection: {0}", args.Socket.Options.Identity.ToString());
-            };
-            
-            command.Options.Identity = Encoding.UTF8.GetBytes("COMMAND");
-            command.Options.RouterMandatory = true;
-            command.Bind(endpointCommand);
-            _testOutputHelper.WriteLine("Bind command");
+
+            var queryExecutor = new QueryExecutor(endpointQuery,null);
 
             Task.Delay(1000);
             
-            var query = new RouterSocket();
-            query.ReceiveReady += (sender, args) =>
-            {
-                _testOutputHelper.WriteLine("New connection: {0}", args.Socket.Options.Identity.ToString());
-            };
-            query.Options.Identity = Encoding.UTF8.GetBytes("QUERY");
-            query.Options.RouterMandatory = true;
-            query.Bind(endpointQuery);
-            _testOutputHelper.WriteLine("Bind query");
+            var queryHandler = new QueryHandler(endpointVentilator,endpointQuery);
             
-            Task.Delay(2000);
-
-            var ventilatorCommand = new DealerSocket();
-            ventilatorCommand.Options.Identity = Encoding.UTF8.GetBytes("VENTILATORCOMMAND");
-            ventilatorCommand.Connect(endpointCommand);
-            _testOutputHelper.WriteLine("Connect to command");
-
-            Task.Delay(1000);
+            Task.Delay(5000);
             
-            var msgCommand = new NetMQMessage();
-            msgCommand.AppendEmptyFrame();
-            msgCommand.Append("HELLO");
-            ventilatorCommand.SendMultipartMessage(msgCommand);
-            _testOutputHelper.WriteLine("Send Hello to command processor");
+            var client = new ClientHandler(endpointVentilator);
             
             Task.Delay(1000);
-          
-            var messageCommandID = command.ReceiveFrameString();
-            command.ReceiveFrameString();
-            var messageCommandCommand = command.ReceiveFrameString();
-            _testOutputHelper.WriteLine("Received Command: {0} {1}", messageCommandID, messageCommandCommand);
-                      
-            Task.Delay(1000);
-
-            var ventilatorQuery = new DealerSocket();
-            ventilatorQuery.Options.Identity = Encoding.UTF8.GetBytes("VENTILATORQUERY");
-            ventilatorQuery.Connect(endpointQuery);
-            _testOutputHelper.WriteLine("Connect to query");
+            
+            client.Send("TESTE");
+            
+             
+        }
+        
+        interface ITransport<TInputSocket, TOutputSocket>: IDisposable 
+                                where TInputSocket: NetMQSocket 
+                                where TOutputSocket: NetMQSocket
+        {
            
-            Task.Delay(1000);
-            
-            var msgQuery = new NetMQMessage();
-            msgQuery.AppendEmptyFrame();
-            msgQuery.Append("HELLO");
-            ventilatorQuery.SendMultipartMessage(msgQuery);
-            _testOutputHelper.WriteLine("Send Hello to query processor");
+        }
 
-            Task.Delay(1000);
+         interface IOutputTransport<TData>
+         {
+             void Send(TData data);
+         }
+
+         public sealed class ClientHandler : IOutputTransport<String>
+         {
+             private readonly String _serverEndpoint;
+
+             public ClientHandler(String serverEndpoint)
+             {
+                 _serverEndpoint = serverEndpoint;
+             }
+             
+             public void Send(String message)
+             {
+                 using (var server = new DealerSocket())
+                 {
+                     server.Options.Identity = Encoding.UTF8.GetBytes("CLIENT");
+
+                     var ok = false;
+                     do
+                     {
+                         try
+                         {
+
+                             server.Connect(_serverEndpoint);
+                             ok = true;
+                             Task.Delay(1000);
+
+                         }
+                         catch (NetMQException ex)
+                         {
+                             
+                         }
+                         
+                     } while (!ok);
+                     
+                     
+                     var msg = new NetMQMessage();
+                     msg.AppendEmptyFrame();
+                     msg.Append(message);
+
+                     server.SendMultipartMessage(msg);
+                     
+                     var result = server.ReceiveMultipartMessage();
+
+                     var te = result[0];
+                 }
+             }
+         }
+
+         public sealed class QueryExecutor : ITransport<RouterSocket, DealerSocket>
+         {
+             private readonly RouterSocket _input;
+             private readonly DealerSocket _output;
+
+             private readonly String _externalEndpoint;
+             private readonly String _statusEndpoint;
+
+             public QueryExecutor(String externalEndpoint, String statusEndpoint)
+             {
+                 _externalEndpoint = externalEndpoint;
+                 _statusEndpoint = statusEndpoint;
+                 
+                 var server = new Task(() => StartExternalEndpoint(),
+                     TaskCreationOptions.LongRunning);
+                
+                 server.Start();
+             }
+             
+             private void StartExternalEndpoint()
+             {
+                 using (var server = new RouterSocket())
+                 {
+                     server.Options.Identity = Encoding.UTF8.GetBytes("QUERYEXECUTOR");
+                     server.Options.RouterMandatory = true;
+                     server.Bind(_externalEndpoint);
+                  
+                     server.ReceiveReady += (sender, args) =>
+                     {
+                         // está se conectando em mim então eu posso me conectar de volta.
+                         
+                         // var id = args.Socket.ReceiveFrameString();
+                         // var comando = args.Socket.ReceiveFrameString();
+                         // var empty = args.Socket.ReceiveFrameString();
+                         // var dados = args.Socket.ReceiveFrameString();
+                     };
+                     
+                     try
+                     {
+                         while (true)
+                         {
+                             var message = server.ReceiveMultipartMessage();
+
+                             var json = message[2].ConvertToString();
+                            
+
+                             //TODO: Executar a query e retornar
+                            
+                             // var query = JsonSerializer.Deserialize<TQuery>(json);
+                             // var result = Handler(query);
+                        
+                             var messageToRouter = new NetMQMessage();
+                              messageToRouter.Append(message[0]);
+                              messageToRouter.AppendEmptyFrame();
+                              messageToRouter.Append("OK");
+                             // messageToRouter.Append(JsonSerializer.Serialize(result));
+
+                             server.SendMultipartMessage(messageToRouter);
+                         }
+                     }
+                     catch (Exception ex)
+                     {
+                    
+                     }
+                 }
+             }
+
+             public void Dispose()
+             {
+                 _input?.Dispose();
+                 _output?.Dispose();
+             }
+         }
+         
+        
+
+        // public abstract class Transport: ITransport<RouterSocket, DealerSocket>
+        // {
+        //     private readonly RouterSocket _input;
+        //     private readonly DealerSocket _output;
+        //
+        //     private readonly String _externalEndpoint;
+        //     
+        //     // protected abstract NetMQMessage  
+        // }
+
+        public sealed class QueryHandler : ITransport<RouterSocket, DealerSocket>
+        {
+            private readonly RouterSocket _input;
+            private readonly DealerSocket _output;
+
+            private readonly String _externalEndpoint;
+            private readonly String _internalEndpoint;
+
+            /// <summary>
+            /// TODO: Implementar um construtor para receber uma instância da interface IConfiguration
+            /// INcluir um construtor para receber uma instancia da interface IConfiguration
+            /// que terá lido o appsettings.json ou appsettings
+            /// </summary>
+            /// <param name="externalEndpoint"></param>
+            public QueryHandler(String externalEndpoint)
+            {
+                _externalEndpoint = externalEndpoint;
+                
+                var server = new Task(StartExternalEndpoint,_externalEndpoint,
+                    CancellationToken.None, TaskCreationOptions.LongRunning);
+                
+                server.Start();                
+            }
+                
+            public QueryHandler(String externalEndpoint, String internalEndpoint)
+                :this(externalEndpoint)
+            {
+                _internalEndpoint = internalEndpoint;
+
+                if (!String.IsNullOrEmpty(_internalEndpoint))
+                {
+                    _output = new DealerSocket();
+                }
+
+            }
             
-            var messageQueryID = query.ReceiveFrameString();
-            query.ReceiveFrameString();
-            var messageQueryCommand = query.ReceiveFrameString();
-            _testOutputHelper.WriteLine("Received Query: {0} {1}", messageQueryID, messageQueryCommand);
-            
-            Task.Delay(1000);
-            
-            var ventilator = new RouterSocket();
-            ventilator.Options.Identity = Encoding.UTF8.GetBytes("VENTILATOR");
-            ventilator.Bind(endpointVentilator);
-            _testOutputHelper.WriteLine("Bind Ventilator");
+            private void StartExternalEndpoint(object? endpoint)
+            {
+                using (var server = new RouterSocket())
+                {
+                    server.Options.Identity = Encoding.UTF8.GetBytes("QUERYHANDLER");
+                    server.Options.RouterMandatory = true;
+                    server.Bind(endpoint.ToString());
+                    var ok = false;
+
+                    do
+                    {
+                        try
+                        {
+                            _output.Connect(_internalEndpoint);
+                            ok = true;
+                        }
+                        catch (NetMQException ex)
+                        {
+
+                        }
+                    } while (!ok);
+
+                    server.ReceiveReady += (sender, args) =>
+                    {
+                        // está se conectando em mim então eu posso me conectar de volta.
+                        
+                        // var id = args.Socket.ReceiveFrameString();
+                        // var comando = args.Socket.ReceiveFrameString();
+                        // var empty = args.Socket.ReceiveFrameString();
+                        // var dados = args.Socket.ReceiveFrameString();
+                    };
+
+                    try
+                    {
+                        while (true)
+                        {
+                            var message = server.ReceiveMultipartMessage();
+
+                            //TODO: roteamento deverá ter vários worker para efetivamente processar os requests
+                            
+                            // var query = JsonSerializer.Deserialize<TQuery>(json);
+                            // var result = Handler(query);
+                            
+                            var message1 = new NetMQMessage();
+                            message1.Append(message[0]);
+                            message1.AppendEmptyFrame();
+                            message1.Append(message[2]);
+                            
+                            _output.SendMultipartMessage(message1);
+
+                            var result = _output.ReceiveMultipartMessage();
+                            var messageToRouter = new NetMQMessage();
+                            messageToRouter.Append(message[0]);
+                            messageToRouter.AppendEmptyFrame();
+                            messageToRouter.Append(result[1]);
+                            
+                            server.SendMultipartMessage(messageToRouter);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                    
+                    }
+                }
+            }
+           
+            public void Dispose()
+            {
+                _input?.Dispose();
+                _output?.Dispose();
+                
+                NetMQConfig.Cleanup();
+                GC.SuppressFinalize(this);
+            }
         }
     }
 }
