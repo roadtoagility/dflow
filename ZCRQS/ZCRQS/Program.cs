e11f924837eb3fafd1976a301a1cce49589c2b22
douglas ramalho <doug.ramalho@gmail.com>
2020-03-15 21:42:52 -0300

    refatorando eventstream, criando um dicionario para os observers

--- ZCRQS/ZCRQS/Program.cs
+++ ZCRQS/ZCRQS/Program.cs
@@ -48,6 +48,7 @@ namespace ZCRQS
     public sealed class EventStream
     {
         private List<IObserverStream> _observers = new List<IObserverStream>();
+        private Dictionary<string, List<IObserverStream>> _observersDictionary = new Dictionary<string, List<IObserverStream>>();
         
         private static readonly Lazy<EventStream>
             lazy =
@@ -62,16 +63,28 @@ namespace ZCRQS
         
         public void Subscribe(IObserverStream observer)
         {
+            foreach (var subscriber in observer.GetEventListers())
+            {
+                if (!_observersDictionary.ContainsKey(subscriber))
+                {
+                    _observersDictionary.Add(subscriber, new List<IObserverStream>());
+                }
+                
+                _observersDictionary[subscriber].Add(observer);
+            }
             _observers.Add(observer);
         }
         
         public void Raise(params Event[] events)
         {
-            foreach (var @eventUnit in events)
+            foreach (var eventUnit in events)
             {
-                foreach (var observer in _observers)
+                if (_observersDictionary.ContainsKey(eventUnit.Name))
                 {
-                    observer.Raise(eventUnit);
+                    foreach (var observer in _observersDictionary[eventUnit.Name])
+                    {
+                        observer.Raise(eventUnit);
+                    }
                 }
             }
         }
@@ -80,6 +93,7 @@ namespace ZCRQS
     public interface IObserverStream
     {
         void Raise(Event eventUnit);
+        string[] GetEventListers();
     }
 
     public class AtualizarCpfCommand
@@ -187,6 +201,7 @@ namespace ZCRQS
         }
         
 
+        //TODO: não poode deixar um raise para todos os tipos de eventos de umm queryhandler, corrigir isso
         public void Raise(Event eventUnit)
         {
             if (eventUnit.Entity is PessoaAggregate)
@@ -205,8 +220,12 @@ namespace ZCRQS
             {
                 
             }
-            
-            
+        }
+
+        //TODO: mover isso, cada query só pode se inscrever em um evento, mover para o construtor o evento e criar um base que implementa o getlisteners
+        public string[] GetEventListers()
+        {
+            return new string[]{"FullPessoaUpdated", "CpfUpdated"};
         }
     }
 
