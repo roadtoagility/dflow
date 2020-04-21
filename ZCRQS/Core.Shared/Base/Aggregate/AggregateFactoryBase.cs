f08c9d626ccf1a3aafbdb881188c8bef3c834c00
douglas ramalho <doug.ramalho@gmail.com>
2020-04-20 21:46:41 -0300

    refatorando factory

--- ZCRQS/Core.Shared/Base/Aggregate/AggregateFactoryBase.cs
+++ ZCRQS/Core.Shared/Base/Aggregate/AggregateFactoryBase.cs
@@ -5,37 +5,16 @@ using Core.Shared.Interfaces;
 
 namespace Core.Shared.Base.Aggregate
 {
-    public class AggregateFactory
+    public abstract class AggregateFactoryBase
     {
         private readonly ISnapshotRepository<Guid> _snapshotRepository;
         private readonly IEventStore<Guid> _eventStore;
-        private readonly long INITIAL_VERSION = 1;
 
         //summay: aplicações não são obrigadas a possuirem uma estrutura de snapshot
-        public AggregateFactory(IEventStore<Guid> eventStore, ISnapshotRepository<Guid> snapshotRepository = null)
+        public AggregateFactoryBase(IEventStore<Guid> eventStore, ISnapshotRepository<Guid> snapshotRepository = null)
         {
             _snapshotRepository = snapshotRepository;
             _eventStore = eventStore;
-        }
-        
-        public TAggregate Create<TAggregate>(Guid? id = null)
-            where TAggregate : AggregateRoot<Guid>
-        {
-            id = id == null || id.Value == Guid.Empty ? Guid.NewGuid() : id;
-            
-            var existStream = _eventStore.Any(id.Value);
-            if(existStream)
-                throw new DuplicatedRootException(id.ToString());
-            
-            var createEvent = new AggregateCreated<Guid>(id.Value);
-            var events = new List<IEvent>() { createEvent};
-            
-            var stream = new EventStream(){ Version = INITIAL_VERSION, Events = events};
-
-            TAggregate aggregate = (TAggregate) Activator.CreateInstance(typeof(TAggregate), new object[] {stream});
-            
-            aggregate.Changes.Add(createEvent);
-            return aggregate;
         }
         
         public TAggregate Load<TAggregate>(Guid id)
---
+++ ZCRQS/ZCRQS/AggregateFactory.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using Core.Shared.Base;
+using Core.Shared.Base.Aggregate;
+using Core.Shared.Base.Exceptions;
+using Core.Shared.Interfaces;
+
+namespace Program
+{
+    public class AggregateFactory : AggregateFactoryBase
+    {
+        private readonly IEventStore<Guid> _eventStore;
+        private readonly long INITIAL_VERSION = 1;
+
+        public AggregateFactory(IEventStore<Guid> eventStore, ISnapshotRepository<Guid> snapshotRepository = null) 
+            : base(eventStore, snapshotRepository)
+        {
+            _eventStore = eventStore;
+        }
+        
+        public TAggregate Create<TAggregate>(Guid? id = null)
+            where TAggregate : AggregateRoot<Guid>
+        {
+            id = id == null || id.Value == Guid.Empty ? Guid.NewGuid() : id;
+            
+            var existStream = _eventStore.Any(id.Value);
+            if(existStream)
+                throw new DuplicatedRootException(id.ToString());
+            
+            var createEvent = new AggregateCreated<Guid>(id.Value);
+            var events = new List<IEvent>() { createEvent};
+            
+            var stream = new EventStream(){ Version = INITIAL_VERSION, Events = events};
+
+            TAggregate aggregate = (TAggregate) Activator.CreateInstance(typeof(TAggregate), new object[] {stream});
+            
+            aggregate.Changes.Add(createEvent);
+            return aggregate;
+        }
+    }
+}
