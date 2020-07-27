6984ccce35779b53571c96c5d4705ea4e33515de
Douglas José Ramalho Araujo <douglas.ramalho@squadra.com.br>
2020-07-27 19:39:04 -0300

    fix SHouldUpdateProductCatalogView

--- DFlow.Example/Handlers/ProductQueryHandler.cs
+++ DFlow.Example/Handlers/ProductQueryHandler.cs
@@ -1,34 +1,34 @@
 using System;
 using System.Collections.Generic;
 using DFlow.Example.Views;
+using DFlow.Interfaces;
 
 namespace DFlow.Example.Handlers
 {
     public class ProductQueryHandler : IProductQueryHandler
     {
         private readonly ViewFactory _factory;
+        private IReadModel<ProductDTO> _view;
 
         public ProductQueryHandler(ViewFactory factory)
         {
             _factory = factory;
+            _view = _factory.CreateProductView();
         }
 
         public IList<ProductDTO> ListAllProducts()
         {
-            var view = _factory.CreateProductView();
-            return view.QueryAll();
+            return _view.QueryAll();
         }
 
         public IList<ProductDTO> ListByFilter(Func<ProductDTO, bool> query)
         {
-            var view = _factory.CreateProductView();
-            return view.QueryAll(query);
+            return _view.QueryAll(query);
         }
 
         public ProductDTO GetById(Guid id)
         {
-            var view = _factory.CreateProductView();
-            return view.Query(x => x.Id == id);
+            return _view.Query(x => x.Id == id);
         }
     }
 }
