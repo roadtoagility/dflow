fbc41d07730ebdf8f3f46818d760a8f4111dd6ed
drr00t <adribeiro@gmail.com>
2020-08-29 11:43:06 -0300

    build fix build

--- build.ps1
+++ build.ps1
@@ -17,27 +17,27 @@ $root = (Get-Item -Path ".\").FullName
 $artifactsPath = $root + "\artifacts"
 if(Test-Path $artifactsPath) { Remove-Item $artifactsPath -Force -Recurse }
 
-# $branch = @{ $true = $env:APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH; $false = @{ $true = $env:APPVEYOR_REPO_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$env:APPVEYOR_REPO_BRANCH -ne $NULL] }[$env:APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH -ne $NULL];
-# $revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:APPVEYOR_BUILD_NUMBER, 10); $false = "local" }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
-# $suffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)))-$revision"}[$branch -eq "master" -and $revision -ne "local"]
+$branch = @{ $true = $env:APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH; $false = @{ $true = $env:APPVEYOR_REPO_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$env:APPVEYOR_REPO_BRANCH -ne $NULL] }[$env:APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH -ne $NULL];
+$revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:APPVEYOR_BUILD_NUMBER, 10); $false = "local" }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
+$suffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)))-$revision"}[$branch -eq "master" -and $revision -ne "local"]
 # $commitHash = $(git rev-parse --short HEAD)
 # $buildSuffix = @{ $true = "$($suffix)-$($commitHash)"; $false = "$($branch)-$($commitHash)" }[$suffix -ne ""]
-# $versionSuffix = @{ $true = "--version-suffix=$($suffix)"; $false = ""}[$suffix -ne ""]
+$versionSuffix = @{ $true = "--version-suffix=$($suffix)"; $false = ""}[$suffix -ne ""]
 
-# echo "Build: Package version suffix is $suffix"
-# echo "Build: Build version suffix is $buildSuffix"
+echo "Build: Package version suffix is $suffix"
+echo "Build: Build version suffix is $buildSuffix"
 
-# # Update Appveyor version
-# if (Test-Path env:APPVEYOR) {
-#     $props = [xml](Get-Content "src\Directory.Build.props")
-#     $prefix = $props.Project.PropertyGroup.VersionPrefix
+# Update Appveyor version
+if (Test-Path env:APPVEYOR) {
+    $props = [xml](Get-Content "src\Directory.Build.props")
+    $prefix = $props.Project.PropertyGroup.VersionPrefix
     
-#     $avSuffix = @{ $true = $($suffix); $false = $props.Project.PropertyGroup.VersionSuffix }[$suffix -ne ""]
-#     $full = @{ $true = "$($prefix)-$($avSuffix)"; $false = $($prefix) }[-not ([string]::IsNullOrEmpty($avSuffix))]
+    $avSuffix = @{ $true = $($suffix); $false = $props.Project.PropertyGroup.VersionSuffix }[$suffix -ne ""]
+    $full = @{ $true = "$($prefix)-$($avSuffix)"; $false = $($prefix) }[-not ([string]::IsNullOrEmpty($avSuffix))]
     
-#     echo "Build: Full version is $full"
-#     Update-AppveyorBuild -Version $full
-# }
+    echo "Build: Full version is $full"
+    Update-AppveyorBuild -Version $full
+}
 
 # Build
 echo "`n`n----- BUILD -----`n"
