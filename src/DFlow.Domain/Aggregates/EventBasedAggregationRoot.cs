565e319f8bfebf4349df7a3a45fbeeb5efb4ee30
drr00t <adribeiro@gmail.com>
2021-03-09 22:15:59 -0300

    implementing event-based aggregation version.

---
+++ src/DFlow.Domain/Aggregates/EventBasedAggregationRoot.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (C) 2020  Road to Agility
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using DFlow.Domain.BusinessObjects;
+using DFlow.Domain.Events;
+using FluentValidation.Results;
+
+namespace DFlow.Domain.Aggregates
+{
+    public class EventBasedAggregationRoot<TEntityId> : IChangeSet<EventStream<TEntityId>>
+    {
+        private readonly TEntityId _aggregateId;
+        private readonly Version _version;
+        private readonly List<IDomainEvent> _changes = new List<IDomainEvent>();
+
+        protected EventBasedAggregationRoot(TEntityId aggregateId, Version version, IImmutableList<IDomainEvent> events)
+        {
+            _aggregateId = aggregateId;
+            _version = version;
+            _changes.AddRange(events);
+        }
+        
+        protected void Apply(IDomainEvent item)
+        {
+            _changes.Add(item);
+        }
+        
+        protected void Raise(IDomainEvent @event)
+        {
+            _changes.Add(@event);
+        }
+        
+        public EventStream<TEntityId> GetChange()
+        {
+            return EventStream<TEntityId>.From(_aggregateId,_version, _changes.ToImmutableList());
+        }
+
+        public IReadOnlyList<IDomainEvent> GetEvents()
+        {
+            return _changes.ToImmutableList();
+        }
+
+        public ValidationResult ValidationResults { get; protected set; }
+
+        public EventBasedAggregationRoot<TEntityId> Create(TEntityId aggregateId, IImmutableList<IDomainEvent> events)
+        {
+            return new EventBasedAggregationRoot<TEntityId>(aggregateId, Version.New(), events);
+        }
+    }
+}
---
+++ src/DFlow.Domain/BusinessObjects/EventStream.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using DFlow.Domain.Events;
+using DFlow.Domain.Validation;
+
+namespace DFlow.Domain.BusinessObjects
+{
+    public class EventStream<TEntityId>: ValidationStatus
+    {
+        private EventStream(TEntityId aggregateId, Version version, IImmutableList<IDomainEvent> events)
+        {
+            AggregationId = aggregateId;
+            Version = version;
+            Events = events;
+        }
+        
+        public TEntityId AggregationId { get; }
+        public Version Version { get; }
+        public IImmutableList<IDomainEvent> Events { get; }
+        
+        public bool IsNew() => Version.Equals(Version.New());
+
+        public static EventStream<TEntityId> From(TEntityId aggregateId, Version version, IImmutableList<IDomainEvent> events)
+        {
+            var eventStream = new EventStream<TEntityId>(aggregateId,version,events);
+            //events.ForEach(ev => { });
+            return eventStream;
+        }
+
+        public static EventStream<TEntityId> AppendStream(EventStream<TEntityId> stream, IImmutableList<IDomainEvent> appendEvents)
+        {
+            var newStream = stream.Events.AddRange(appendEvents);
+            return From(stream.AggregationId, Version.Next(stream.Version), newStream);
+        }
+        
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return AggregationId;
+            yield return Version;
+        }
+    }
+}
