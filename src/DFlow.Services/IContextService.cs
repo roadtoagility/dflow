2573b39f70fcb32edbe2a9f3ed2bb32577d5b548
drr00t <adribeiro@gmail.com>
2021-05-16 22:08:51 -0300

    initial thinking about background tasks:  - allow supervized background services execution for more reliable microservices

---
+++ src/DFlow.Services/IContextService.cs
@@ -0,0 +1,17 @@
+// Copyright (C) 2020  Road to Agility
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DFlow.Services
+{
+    public interface IContextService
+    {
+        void AddService(IDFlowService service);
+        void UpdateService(IDFlowService service);
+    }
+}
