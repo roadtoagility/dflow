a1994721a414d547e5c8e9151927bffef1a59163
drr00t <adribeiro@gmail.com>
2021-07-24 11:46:33 -0300

    Use ValueOf implementation:  - from Harry McIntyre ValueOf project

---
+++ src/DFlow.Domain/BusinessObjects/ValueOf.cs
@@ -0,0 +1,119 @@
+﻿// Copyright (c) 2021 Harry McIntyre
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+// of the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace DFlow.Domain.BusinessObjects
+{
+    public class ValueOf<TValue, TThis, TValidator> 
+        where TThis : ValueOf<TValue, TThis, TValidator>
+        where TValidator: AbstractValidator<TThis>, new()
+    {
+        public ValidationResult ValidationStatus { get; private set; }
+
+        private static readonly Func<TThis> Factory;
+
+        /// <summary>
+        /// WARNING - THIS FEATURE IS EXPERIMENTAL. I may change it to do
+        /// validation in a different way.
+        /// Right now, override this method, and throw any exceptions you need to.
+        /// Access this.Value to check the value
+        /// </summary>
+        protected virtual void Validate()
+        {
+            ValidationStatus = new TValidator().Validate((TThis)this);
+        }
+
+        static ValueOf()
+        {
+            ConstructorInfo ctor = typeof(TThis)
+                .GetTypeInfo()
+                .DeclaredConstructors
+                .First();
+
+            var argsExp = new Expression[0];
+            NewExpression newExp = Expression.New(ctor, argsExp);
+            LambdaExpression lambda = Expression.Lambda(typeof(Func<TThis>), newExp);
+
+            Factory = (Func<TThis>)lambda.Compile();
+        }
+
+        public TValue Value { get; protected set; }
+
+        public static TThis From(TValue item)
+        {
+            TThis x = Factory();
+            x.Value = item;
+            x.Validate();
+
+            return x;
+        }
+
+        protected virtual bool Equals(ValueOf<TValue, TThis,TValidator> other)
+        {
+            return EqualityComparer<TValue>.Default.Equals(Value, other.Value);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null)
+                return false;
+
+            if (ReferenceEquals(this, obj))
+                return true;
+
+            return obj.GetType() == GetType() && Equals((ValueOf<TValue, TThis, TValidator>)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return EqualityComparer<TValue>.Default.GetHashCode(Value);
+        }
+
+        public static bool operator ==(ValueOf<TValue, TThis, TValidator> a, ValueOf<TValue, TThis, TValidator> b)
+        {
+            if (a is null && b is null)
+                return true;
+
+            if (a is null || b is null)
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(ValueOf<TValue, TThis, TValidator> a, ValueOf<TValue, TThis, TValidator> b)
+        {
+            return !(a == b);
+        }
+
+        // Implicit operator removed. See issue #14.
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+}
---
+++ tests/DFlow.Tests/Domain/ValueOfTests.cs
@@ -0,0 +1,76 @@
+﻿// Copyright (C) 2020  Road to Agility
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+
+using System;
+using AutoFixture;
+using DFlow.Domain.BusinessObjects;
+using DFlow.Tests.Supporting.DomainObjects;
+using FluentValidation;
+using Xunit;
+
+namespace DFlow.Tests.Domain
+{
+    public sealed class ValueOfTests
+    {
+        [Fact]
+        public void ValueOf_create_Vo_isValid()
+        {
+            var vo = Vo.From("teste");
+
+            Assert.True(vo.ValidationStatus.IsValid);
+        }
+
+        [Fact]
+        public void ValueOf_create_Vo_not_Valid()
+        {
+            var vo = Vo.From("");
+
+            Assert.False(vo.ValidationStatus.IsValid);
+        }
+
+        [Fact]
+        public void ValueOf_create_ComplexVo_isValid()
+        {
+            var vo = ComplexVo.From(("teste", Vo.From("outro vo")));
+
+            Assert.True(vo.ValidationStatus.IsValid);
+        }
+
+        [Fact]
+        public void ValueOf_create_ComplexVo_not_Valid()
+        {
+            var vo = ComplexVo.From(("", Vo.From("")));
+
+            Assert.False(vo.ValidationStatus.IsValid);
+        }
+        
+        class ComplexVo : ValueOf<(string,Vo), ComplexVo, ComplexVoValidator>
+        {
+        }
+        
+        class ComplexVoValidator : AbstractValidator<ComplexVo>
+        {
+            public ComplexVoValidator()
+            {
+                RuleFor(x => x.Value.Item1).NotEmpty();
+                RuleFor(x => x.Value.Item1).NotNull();
+                RuleFor(x => x.Value.Item2).SetValidator(new VoValidator());
+            }   
+        }
+        class Vo : ValueOf<string, Vo, VoValidator>
+        {
+        }
+        class VoValidator : AbstractValidator<Vo>
+        {
+            public VoValidator()
+            {
+                RuleFor(x => x.Value).NotEmpty();
+                RuleFor(x => x.Value).NotNull();
+            }   
+        }
+    }
+}
