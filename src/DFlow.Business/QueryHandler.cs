7d6f7ad5ebbc79fb1c4f64177195cdc2580b181f
drr00t <adribeiro@gmail.com>
2022-03-04 16:12:29 -0300

    business eventbus

---
+++ src/DFlow.Business/QueryHandler.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (C) 2020  Road to Agility
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+using System.Threading;
+using System.Threading.Tasks;
+using DFlow.Business.Cqrs.CommandHandlers;
+
+namespace DFlow.Business.Cqrs
+{
+    public abstract class QueryHandler<TFilter, TResult> : ICommandHandler<TFilter, TResult>
+    {
+      
+        public async Task<TResult> Execute(TFilter filter)
+        {
+            var cancellationToken = new CancellationTokenSource();
+            return await Execute(filter, cancellationToken.Token)
+                .ConfigureAwait(false);
+        }
+        
+        public async Task<TResult> Execute(TFilter filter, CancellationToken cancellationToken)
+        {
+            return await ExecuteQuery(filter, cancellationToken)
+                .ConfigureAwait(false);
+        }
+
+        protected abstract Task<TResult> ExecuteQuery(TFilter filter, CancellationToken cancellationToken);
+    }
+}
---
+++ src/contrib/DFlow.Business.MessageBus.FluentMediator/FluentMediatorDomainEventBus.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (C) 2020  Road to Agility
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+using System.Threading;
+using System.Threading.Tasks;
+using FluentMediator;
+
+namespace DFlow.Business.MessageBus.FluentMediator
+{
+    public class MessageBus:IBusinessMessageBus
+    {
+        private readonly IMediator _mediator;
+        
+        public MessageBus(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task Publish<TEvent>(TEvent request)
+        {
+            var cancellation = new CancellationToken();
+            await Publish(request, cancellation).ConfigureAwait(false);
+        }
+
+        public async Task Publish<TEvent>(TEvent request, CancellationToken cancellationToken)
+        {
+            await _mediator.PublishAsync(request, cancellationToken).ConfigureAwait(false);
+        }
+
+        public async Task<TResult> Send<TResult,TRequest>(TRequest request)
+        {
+            var cancellation = new CancellationToken();
+            return await Send<TResult, TRequest>(request, cancellation)
+                .ConfigureAwait(false);
+        }
+
+        public async Task<TResult> Send<TResult, TRequest>(TRequest request, CancellationToken cancellationToken)
+        {
+            return await _mediator.SendAsync<TResult>(request, cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
