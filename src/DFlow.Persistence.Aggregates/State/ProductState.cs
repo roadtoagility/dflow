7947050c0ed3a5b2dd0260efbc33ff4efe2e9137
drr00t <adribeiro@gmail.com>
2023-02-05 13:18:19 -0300

    removing:  - implmentation specifics project from core

--- src/DFlow.Persistence.Aggregates/State/ProductState.cs
+++
@@ -1,5 +0,0 @@
-namespace Ecommerce.Persistence.State;
-
-public sealed record ProductState(Guid Id, string Name, string Description
-        , double Weight, byte[] RowVersion)
-    : State(RowVersion);
--- src/DFlow.Persistence.Aggregates/State/State.cs
+++
@@ -1,6 +0,0 @@
-namespace Ecommerce.Persistence.State;
-
-public record State(byte[] RowVersion)
-{
-    public bool IsDeleted { get; set; }
-}
